e5d2d54fabfd01038b2c74162de4d58cd17ca272
Pete Holiday <pete.holiday@gmail.com>
2013-07-09 18:24:56 -0400

    Ant movement, Antfarm abstraction.

--- antfarm.py
+++ antfarm.py
@@ -21,11 +21,9 @@ show_panel = False
 
 # Farm dimensions
 farm = Farm(160, 120)
+farm.viewport.size(SCREEN_WIDTH, SCREEN_HEIGHT)
+farm.viewport.move(int((farm.width/2)-(SCREEN_WIDTH/2)), 0)
 entities = []
-
-# Camera Offsets
-camera_x = int((farm.width/2)-(SCREEN_WIDTH/2))
-camera_y = 0
 
 # More globals, because I love them
 mouse = libtcod.Mouse()
@@ -37,9 +35,12 @@ for file in os.listdir(b'data/namegen') :
         libtcod.namegen_parse(os.path.join(b'data',b'namegen',file))
 
 def handle_keys():
-    global farm, key, mouse, camera_x, camera_y, panel_console, show_panel, entities
+    global farm, key, mouse, panel_console, show_panel, entities
  
     if key.vk != libtcod.KEY_NONE:
+        viewport_dx = 0
+        viewport_dy = 0
+
         # vertical movement
         for case in switch(key.vk):
             if case(libtcod.KEY_UP): pass
@@ -48,7 +49,7 @@ def handle_keys():
             if case(libtcod.KEY_KP7): pass
             if case(libtcod.KEY_PAGEUP): pass
             if case(libtcod.KEY_KP9):
-                camera_y -= 1
+                viewport_dy -= 1
                 break
             if case(libtcod.KEY_DOWN): pass
             if case(libtcod.KEY_KP2): pass
@@ -56,7 +57,7 @@ def handle_keys():
             if case(libtcod.KEY_KP1): pass
             if case(libtcod.KEY_PAGEDOWN): pass
             if case(libtcod.KEY_KP3):
-                camera_y += 1
+                viewport_dy += 1
                 break
 
         # horizontal movement
@@ -67,7 +68,7 @@ def handle_keys():
             if case(libtcod.KEY_KP7): pass
             if case(libtcod.KEY_END): pass
             if case(libtcod.KEY_KP1):
-                camera_x -= 1
+                viewport_dx -= 1
                 break
             if case(libtcod.KEY_RIGHT): pass
             if case(libtcod.KEY_KP6): pass
@@ -75,8 +76,11 @@ def handle_keys():
             if case(libtcod.KEY_KP9): pass
             if case(libtcod.KEY_PAGEDOWN): pass
             if case(libtcod.KEY_KP3):
-                camera_x += 1
+                viewport_dx += 1
                 break
+
+        if viewport_dx != 0 or viewport_dy != 0:
+            farm.viewport.move(viewport_dx, viewport_dy)
 
         if key.vk == libtcod.KEY_ESCAPE:
             return True  #exit game
@@ -88,23 +92,21 @@ def handle_keys():
         else:
             key_char = chr(key.c)
             for case in switch(key_char):
-                if case('?'):
+                if case('i'):
                     show_panel = not show_panel
 
     if mouse.lbutton_pressed:
-        (x,y) = (mouse.cx + camera_x,mouse.cy + camera_y)
+        (x,y) = (mouse.cx + farm.viewport.x, mouse.cy + farm.viewport.y)
         entity_list = []
 
         # Check for an ant?
         for entity in entities:
             if entity.x == x and entity.y == y:
-                entity_list.append(str.format("{}\n---------------\nAnt ID: {}\nPosition: ({},{})", entity.name, id(entity), x, y))
+                entity_list.append(str.format("{}\n---------------\nAnt ID: {}\nPosition: ({},{})\nState: {}", entity.name, id(entity), x, y, entity.state))
 
-        if len(entity_list) == 0:
-            entity_list.append(str.format("There are no ants at ({},{}).\n", x, y))
-
-        libtcod.console_clear(panel_console)
-        libtcod.console_print_rect(panel_console, 2, 2, PANEL_WIDTH-4, 10, "\n\n".join(entity_list))
+        if len(entity_list) != 0:
+            libtcod.console_clear(panel_console)
+            libtcod.console_print_rect(panel_console, 2, 2, PANEL_WIDTH-4, 10, "\n\n".join(entity_list))
 
 def place_ants(number):
     global farm, entities
@@ -113,7 +115,7 @@ def place_ants(number):
     if len(spots) > 0:
         for i in range(number):
             (x, y) = choice(spots)
-            entities.append(Ant(x, y, '@', libtcod.black, libtcod.namegen_generate(choice(libtcod.namegen_get_sets()))))
+            entities.append(Ant(x, y, '@', libtcod.black, farm, libtcod.namegen_generate(choice(libtcod.namegen_get_sets()))))
 
 
 libtcod.console_set_custom_font('data/fonts/lord_dullard_12x12.png', libtcod.FONT_LAYOUT_ASCII_INROW)
@@ -128,8 +130,17 @@ farm.place_entities(entities)
 libtcod.console_clear(panel_console)
 
 while not libtcod.console_is_window_closed():
+    # tick ants
+    for entity in entities:
+        entity.tick()
+
+    farm.update()
+    farm.place_entities(entities)
+
     libtcod.console_clear(0)
-    libtcod.console_blit(farm.console, camera_x, camera_y, SCREEN_WIDTH, SCREEN_HEIGHT, 0, 0, 0)
+    (vpx,vpy) = (farm.viewport.x, farm.viewport.y)
+    (vph,vpw) = farm.viewport.size()
+    libtcod.console_blit(farm.console, vpx, vpy, vph, vpw, 0, 0, 0)
     if show_panel:
         libtcod.console_blit(panel_console, 0, 0, PANEL_WIDTH, PANEL_HEIGHT, 0, SCREEN_WIDTH - PANEL_WIDTH, 0, 1, .9)
     libtcod.console_flush()
--- objects.py
+++ objects.py
@@ -1,17 +1,46 @@
 import libtcodpy as libtcod
+from random import choice
+from lib import switch
 
 class Ant:
-    def __init__(self, x, y, char, color, name='Ant-nonymous'):
-        self.name = name
+    def __init__(self, x, y, char, color, farm, name='Ant-nonymous'):
+        self.state = 'rest'
+        self.state_counter = 0
+        self.cooldown = 0
+
         self.x = x
         self.y = y
         self.char = char
         self.color = color
+        self.farm = farm
+        self.name = name
  
     def move(self, dx, dy):
         #move by the given amount
-        self.x += dx
-        self.y += dy
+        if not self.farm.solid(self.x+dx, self.y+dy):
+            self.x += dx
+            self.y += dy
+
+    def tick(self):
+        self.state_counter += 1
+
+        if self.cooldown > 0:
+            self.cooldown -= 1
+            return
+        
+        for case in switch(self.state):
+            if case('rest'):
+                if self.state_counter > 100:
+                    self.state_counter = 0
+                    self.state = 'wander'
+                break
+            if case('wander'):
+                if self.state_counter > 100:
+                    self.state_counter = 0
+                    self.state = 'rest'
+                    return
+
+                self.move(choice([-1,0,1]),0)
 
 class Food:
     def __init__(self):
@@ -22,6 +51,7 @@ class Food:
 
 class Dirt:
     char_options = [' ', 176, 177, 178]
+    solid = True
     def __init__(self, char_selection = 0):
         self.char = self.char_options[abs(int(char_selection))]
         # http://www.colourlovers.com/palette/1254353/Reflections
@@ -29,12 +59,14 @@ class Dirt:
         self.fg_color = libtcod.Color(89,54,12)
 
 class Wall:
+    solid = True
     def __init__(self):
         self.char = ' '
         self.bg_color = libtcod.Color(0,168,0)
         self.fg_color = libtcod.white
 
 class Air:
+    solid = False
     def __init__(self):
         self.char = ' '
         self.bg_color = libtcod.Color(212,255,255)
@@ -42,10 +74,37 @@ class Air:
 
 
 class Farm:
+    class Viewport:
+        def __init__(self, x, y, height, width):
+            self.x = x
+            self.y = y
+            self.height = height
+            self.width = width
+
+        def move(self, dx, dy):
+            self.x += dx
+            self.y += dy
+
+        def size(self, height = None, width = None):
+            if height != None:
+                self.height = height
+
+            if width != None:
+                self.width = width
+
+            return (height, width)
+
+        def bounding_box(self):
+            return (self.x, self.y, self.x + self.width, self.y + self.height)
+
     def __init__(self, height, width):
         self.height = height
         self.width = width
+        self.viewport = Farm.Viewport(0,0,height,width)
         self.console = libtcod.console_new(height, width)
+
+    def solid(self, x, y):
+        return self.tiles[x][y].solid
 
     def generate(self, border_size = 1, air_buffer = 10):
         noise_2d = libtcod.noise_new(2)
@@ -68,10 +127,9 @@ class Farm:
         locations = []
         for x in range(self.width):
             for y in range(self.height):
-                if isinstance(self.tiles[x][y], Air):
-                    tile_below = self.tiles[x][y+1]
-                    if isinstance(tile_below, Dirt) or isinstance(tile_below, Wall):
-                        locations.append((x,y))
+                if not self.solid(x,y) and self.solid(x,y+1):
+                    locations.append((x,y))
+
         return locations
 
     def place_entities(self, entity_list):
